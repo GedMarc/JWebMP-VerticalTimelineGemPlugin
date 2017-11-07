00adfc6fdfde58a23e2c757e8960419ba38bc222
Marc Magon <ged_marc@hotmail.com>
2017-11-07 02:43:25 +0200

    Coverage and Test Cases

--- src/za/co/mmagon/jwebswing/components/verticaltimeline/VerticalTimelineAttributes.java
+++ src/za/co/mmagon/jwebswing/components/verticaltimeline/VerticalTimelineAttributes.java
@@ -17,6 +17,7 @@
 package za.co.mmagon.jwebswing.components.verticaltimeline;
 
 import za.co.mmagon.jwebswing.base.html.interfaces.AttributeDefinitions;
+import za.co.mmagon.jwebswing.utilities.StaticStrings;
 
 /**
  * @author GedMarc
@@ -44,6 +45,6 @@ public enum VerticalTimelineAttributes implements AttributeDefinitions
 	@Override
 	public String toString()
 	{
-		return name().toLowerCase().replace('_', '-').replace("$", "");
+		return name().toLowerCase().replace(StaticStrings.CHAR_UNDERSCORE, '-').replace("$", "");
 	}
 }
