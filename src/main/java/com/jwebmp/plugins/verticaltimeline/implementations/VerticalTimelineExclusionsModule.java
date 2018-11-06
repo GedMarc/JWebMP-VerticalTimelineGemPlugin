d6aff71b4ee40621e34517d93262d285ca69f34e
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:28:42 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/verticaltimeline/implementations/VerticalTimelineExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.verticaltimeline.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class VerticalTimelineExclusionsModule
+		implements IGuiceScanModuleExclusions<VerticalTimelineExclusionsModule>,
+				           IGuiceScanJarExclusions<VerticalTimelineExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-vertical-timeline-css-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.verticaltimeline");
+		return strings;
+	}
+}
