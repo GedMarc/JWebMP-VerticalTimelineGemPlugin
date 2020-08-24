a955193ff16165cb7f4e29984aa9bedcd9bb3eeb
GedMarc <ged_marc@hotmail.com>
2020-08-25 01:16:29 +0200

    All version updates

--- src/main/java/com/jwebmp/plugins/verticaltimeline/VerticalTimelineAttributes.java
+++ src/main/java/com/jwebmp/plugins/verticaltimeline/VerticalTimelineAttributes.java
@@ -19,6 +19,7 @@ package com.jwebmp.plugins.verticaltimeline;
 import com.jwebmp.core.base.html.interfaces.AttributeDefinitions;
 import com.jwebmp.core.utilities.StaticStrings;
 
+import static com.guicedee.guicedinjection.json.StaticStrings.*;
 import static com.jwebmp.core.utilities.StaticStrings.*;
 
 /**
@@ -48,7 +49,7 @@ public enum VerticalTimelineAttributes
 	public String toString()
 	{
 		return name().toLowerCase()
-		             .replace(StaticStrings.CHAR_UNDERSCORE, CHAR_DASH)
-		             .replace("$", "");
+		             .replace(CHAR_UNDERSCORE, CHAR_DASH)
+		             .replace(STRING_DOLLAR, STRING_EMPTY);
 	}
 }
