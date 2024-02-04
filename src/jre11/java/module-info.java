48d546ef428bacbbc9113bd2887d4a20f9fd4cd5
Marc Magon <marc.magon@gmail.com>
2024-02-04 23:42:21 +0200

    Updates for version 2

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -5,7 +5,7 @@ module com.jwebmp.plugins.verticaltimeline {
 	exports com.jwebmp.plugins.verticaltimeline;
 
 	requires com.jwebmp.core;
-	requires com.guicedee.logmaster;
+	
 
 	requires jakarta.validation;
 	requires java.logging;
